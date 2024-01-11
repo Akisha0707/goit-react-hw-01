bd901a6ddf2f525a7229c6e4662a9e3d7a376b80
Akisha0707 <139120038+Akisha0707@users.noreply.github.com>
2024-01-11 21:32:27 +0200

    first

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+// import css from "./App.module.css";
+import Profile from './Profile.jsx';
+import userData from '../userData.json';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
