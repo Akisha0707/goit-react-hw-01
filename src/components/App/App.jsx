c3ca37f60f1266f60aad2e73003386c8a00657db
Akisha0707 <Sakimova@gmail.com>
2024-01-23 21:48:49 +0200

    changes files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,55 @@
-import './App.module.css';
+import css from './App.module.css';
 import {useState } from 'react'
 import Description from '../Description/Description';
-import Feedback from '../Feedback/Feedback';
+// import Feedback from '../Feedback/Feedback';
 import Options from '../Options/Options';
 
 
 
 const App = () => {
-  const [{good, neutral, bad}, setClick] = userState(0)
-  // const [{ good, neutral, bad }] = userState(0);
+
+  // const [count, setCount] = useState(0);
+
+  const [clicks, setClicks] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
+
+  const changeClicksGood = () => {
+    setClicks({
+      ...clicks,
+      good: clicks.good + 1,
+   })
+  }
   
+  const changeClicksNeutal = () => {
+    setClicks({
+      ...clicks,
+      neutral: clicks.neutral + 1,
+   })
+  }
+  
+  const changeClicksBad = () => {
+    setClicks({
+      ...clicks,
+     bad: clicks.bad + 1
+    })
+  }
+
   return (
     <>
       <Description />
-      <Options />
-      <Feedback />
+      <Options  />
+      <div>
+        <ul className={css.options }>
+          <li onClick={changeClicksGood}> Good: {clicks.good}</li>
+          <li onClick={changeClicksNeutal}> Neutral: {clicks.neutral}</li>
+          <li onClick={changeClicksBad}> Bad: {clicks.bad}</li>
+        {/* <li className={css.feedbackTotal}>Total: {good + neutral + bad}</li> */}
+        {/* <li className={css.feedbackPositive}>Positive: {good + neutral}</li> */}
+      </ul>
+        </div>
 
     </>
   );
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,11 +1,20 @@
 import css from './Options.module.css';
+import { useState, useEffect } from 'react'
 
-const Options = ({  }) => {
+const Options = () => {
+  // const [clicks, setClicks] = useState({ good, neutral, bad });
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = (evt) => {
+    setClicks (clicks + 1);
+    console.log('hello');
+  }
+  
   return (
       <div className={css.button}>
-      <button className={css.buttonGood}>Good</button>
-      <button className={css.buttonNeutral}>Neutral</button>
-      <button className={css.buttonBad}>Bad</button>
+      <button className={css.buttonGood} onClick={handleClick}>Good</button>
+      <button className={css.buttonNeutral} onClick={handleClick}>Neutral</button>
+      <button className={css.buttonBad} onClick={handleClick}>Bad</button>
     </div>
   );
 };
